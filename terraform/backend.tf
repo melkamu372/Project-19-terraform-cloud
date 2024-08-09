0588545585dd4276e5171a90067ccfd878056861
melkamu372 <melkamu372@gmail.com>
2024-08-09 16:54:02 +0300

    add providers

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,20 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "melkamu-dev-terraform-bucket"
+#     key            = "global/s3/terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#   }
+# }
+
+
 terraform {
-  backend "s3" {
-    bucket         = "melkamu-dev-terraform-bucket"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+  backend "remote" {
+    organization = "melkamu-pbl-19"
+
+    workspaces {
+      name = "Packer-ami-pbl-19"
+    }
   }
 }
